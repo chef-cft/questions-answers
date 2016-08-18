f6eae3a8891d7a1bc530cbb3518d59137c5132fe
Joe Gardiner <joe@grdnr.io>
2016-08-18 09:14:22 +0100

    Initialised app deploy cookbook

---
+++ cookbooks/qanda-new-app-deploy/metadata.rb
@@ -0,0 +1,7 @@
+name 'qanda-new-app-deploy'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures qanda-new-app-deploy'
+long_description 'Installs/Configures qanda-new-app-deploy'
+version '0.1.0'
---
+++ cookbooks/qanda-new-app-deploy/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: qanda-new-app-deploy
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
