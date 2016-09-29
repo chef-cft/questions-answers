0402fe0184e04cc15e45c38e667a6a1751f39c7e
chef <chef@chef.io>
2016-09-29 13:55:27 +0100

    Removed log steps from deploy.rb:

--- .delivery/build_cookbook/recipes/deploy.rb
+++ .delivery/build_cookbook/recipes/deploy.rb
@@ -6,16 +6,12 @@
 
 search_query = "recipes:#{node['delivery']['change']['project']}* AND chef_environment:#{delivery_environment} " 
 
-log "Search query: #{search_query}"
-
 my_nodes = []
 DeliverySugar::ChefServer.new('C:/delivery/ws/.chef/knife.rb').with_server_config do
   ::Chef::Search::Query.new.search(:node, search_query) { |o| my_nodes << o }
 end
  
 my_nodes.map!(&:name)
-
-log "My nodes: #{my_nodes}"
 
 delivery_push_job "deploy_#{node['delivery']['change']['project']}" do
   command 'chef-client'
