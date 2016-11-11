c3333c8d6abf0b0e540b2c049ffcaf592e10efaf
chef <chef@chef.io>
2016-11-11 14:14:22 +0000

    fixed typo in default.rb

--- cookbooks/questions-and-answers-new-app-deploy/recipes/default.rb
+++ cookbooks/questions-and-answers-new-app-deploy/recipes/default.rb
@@ -49,12 +49,12 @@ end
 
 app_data = data_bag_item('questions-and-answers', 'app_details')
 
-directory node'application']['staging_dir'] do
+directory node['application']['staging_dir'] do
   action :delete
   recursive true
 end
 
-windows_zipfile node'application']['staging_dir'] do
+windows_zipfile node['application']['staging_dir'] do
   source app_data['artifact_location']
   action :unzip
 end
