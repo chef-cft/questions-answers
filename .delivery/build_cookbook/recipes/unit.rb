ff6adb6d07325c039d4ab57e2f3154dfa009fdeb
chef <chef@chef.io>
2016-12-05 13:50:40 +0000

    Added in unit and syntax checks for application

--- .delivery/build_cookbook/recipes/unit.rb
+++ .delivery/build_cookbook/recipes/unit.rb
@@ -5,3 +5,17 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe 'delivery-truck::unit'
+
+# Build and Debug Package
+execute 'running msbuild debug' do
+  command '"C:/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" ./ModuleZeroSampleProject.sln /p:VisualStudioVersion=14.0'
+  cwd "#{node['delivery']['workspace']['repo']}/src"
+  action :run
+end
+
+# Running unit tests
+execute 'running vsconsole tests' do
+  command '"C:/Program Files (x86)/Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" .\Tests\ModuleZeroSampleProject.Tests\bin\Debug\ModuleZeroSampleProject.Tests.dll /TestAdapterPath:"."'
+  cwd "#{node['delivery']['workspace']['repo']}/src"
+  action :run
+end
