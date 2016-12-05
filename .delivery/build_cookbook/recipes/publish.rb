c9563cf6b64343576d0dc98bd2941b91cda3c6cc
chef <chef@chef.io>
2016-12-05 13:38:07 +0000

    Updated publish recipe

--- .delivery/build_cookbook/recipes/publish.rb
+++ .delivery/build_cookbook/recipes/publish.rb
@@ -16,7 +16,7 @@ log "Building artifact from #{node['delivery']['workspace']['repo']}"
 
 # Build Package
 execute 'running msbuild' do
-  command "'C:/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe' ./ModuleZeroSampleProject.sln /p:VisualStudioVersion=14.0 /p:PublishProfile=Release /p:DeployOnBuild=true"
+  command '"C:/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" ./ModuleZeroSampleProject.sln /p:VisualStudioVersion=14.0 /p:PublishProfile=Release /p:DeployOnBuild=true'
   cwd "C:/delivery/ws/aut-workflow-server/Automate/automate-org/questions-and-answers/master/build/publish/repo/src"
   action :run
 end
