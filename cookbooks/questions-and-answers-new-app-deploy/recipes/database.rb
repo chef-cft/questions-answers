a111c8eb3d9ccb89fa220725d3bb6adb268a0396
chef <chef@chef.io>
2016-12-07 15:30:21 +0000

    Test removing database creation resource

--- cookbooks/questions-and-answers-new-app-deploy/recipes/database.rb
+++ cookbooks/questions-and-answers-new-app-deploy/recipes/database.rb
@@ -3,10 +3,10 @@ unless Dir.exist? 'c:/Program Files/Microsoft SQL Server'
   include_recipe 'sql_server::server'
 end
 
-execute "create #{node['database']['name']} database" do
-  command "sqlcmd -Q \"CREATE DATABASE #{node['database']['name']}\""
-  action :run
-end
+#execute "create #{node['database']['name']} database" do
+#  command "sqlcmd -Q \"CREATE DATABASE #{node['database']['name']}\""
+#  action :run
+#end
 
 cookbook_file 'C:/inetpub/wwwroot/qanda/bin/migrate.exe' do
   source 'migrate.exe'
